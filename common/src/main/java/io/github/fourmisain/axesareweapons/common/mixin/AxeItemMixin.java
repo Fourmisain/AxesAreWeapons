65421f6c68c64659d5aa6743ded8812b8a3c5eb3
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-11-08 14:26:43 +0100

    replace cancellable Inject with ModifyConstant

--- common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/AxeItemMixin.java
+++ common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/AxeItemMixin.java
@@ -1,17 +1,12 @@
 package io.github.fourmisain.axesareweapons.common.mixin;
 
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.AxeItem;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AxeItem.class)
 public abstract class AxeItemMixin extends MiningToolItemMixin {
 	@Override
-	public void disableIncreasedDurabilityLoss(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfoReturnable<Boolean> cir) {
-		stack.damage(1, attacker, ((e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND)));
-		cir.setReturnValue(true);
+	public int disableIncreasedAxeDurabilityLoss(int damageAmount) {
+		return 1;
 	}
 }
--- common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/MiningToolItemMixin.java
+++ common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/MiningToolItemMixin.java
@@ -1,17 +1,14 @@
 package io.github.fourmisain.axesareweapons.common.mixin;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(MiningToolItem.class)
 public abstract class MiningToolItemMixin {
-	@Inject(method = "postHit", at = @At("HEAD"), cancellable = true)
-	public void disableIncreasedDurabilityLoss(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfoReturnable<Boolean> cir) {
-		// overwritten by AxeItemMixin
+	@ModifyConstant(method = "postHit", constant = @Constant(intValue = 2))
+	public int disableIncreasedAxeDurabilityLoss(int damageAmount) {
+		return damageAmount; // overwritten by AxeItemMixin
 	}
 }
