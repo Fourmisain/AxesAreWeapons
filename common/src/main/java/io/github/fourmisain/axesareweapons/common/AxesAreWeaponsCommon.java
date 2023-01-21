6ac2010429fec64c322bda8d95ed821ba5f1d0c2
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-01-21 19:50:52 +0100

    backport 1.6.5

--- common/src/main/java/io/github/fourmisain/axesareweapons/common/AxesAreWeaponsCommon.java
+++ common/src/main/java/io/github/fourmisain/axesareweapons/common/AxesAreWeaponsCommon.java
@@ -6,12 +6,17 @@ import net.minecraft.block.Blocks;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.MiningToolItem;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class AxesAreWeaponsCommon {
 	public static final String MOD_ID = "axesareweapons";
 
 	public static AxesAreWeaponsConfig CONFIG;
+
+	public static Identifier id(String id) {
+		return new Identifier(MOD_ID, id);
+	}
 
 	public static boolean isToolWeapon(Item item) {
 		return item instanceof AxeItem || (CONFIG.allToolsAreWeapons && item instanceof MiningToolItem);
@@ -24,10 +29,12 @@ public class AxesAreWeaponsCommon {
 		return 15f;
 	}
 
-	public static void overrideCobWebSuitableness(Item item, BlockState state, CallbackInfoReturnable<Boolean> cir) {
-		if (!CONFIG.fastCobWebBreaking || state.getBlock() != Blocks.COBWEB || !isToolWeapon(item))
-			return;
+	public static boolean overrideCobWebSuitableness(Item item, BlockState state) {
+		return CONFIG.fastCobWebBreaking && state.getBlock() == Blocks.COBWEB && isToolWeapon(item);
+	}
 
-		cir.setReturnValue(true);
+	public static void overrideCobWebSuitableness(Item item, BlockState state, CallbackInfoReturnable<Boolean> cir) {
+		if (overrideCobWebSuitableness(item, state))
+			cir.setReturnValue(true);
 	}
 }
--- fabric/src/main/java/io/github/fourmisain/axesareweapons/fabric/mixin/MiningToolItemMixin.java
+++ fabric/src/main/java/io/github/fourmisain/axesareweapons/fabric/mixin/MiningToolItemMixin.java
@@ -1,8 +1,7 @@
-package io.github.fourmisain.axesareweapons.common.mixin;
+package io.github.fourmisain.axesareweapons.fabric.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,25 +13,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import static io.github.fourmisain.axesareweapons.common.AxesAreWeaponsCommon.overrideCobWebMiningSpeed;
 import static io.github.fourmisain.axesareweapons.common.AxesAreWeaponsCommon.overrideCobWebSuitableness;
 
-@Mixin(value = Item.class, priority = 990)
-public abstract class ItemMixin {
+@Mixin(value = MiningToolItem.class)
+public abstract class MiningToolItemMixin extends ToolItem implements Vanishable {
+	// Note: needs to be a ThreadLocal since some mods call these methods in parallel!
 	@Unique
-	BlockState axesareweapons$state;
+	private final ThreadLocal<BlockState> axesareweapons$state = ThreadLocal.withInitial(() -> null);
+
+	public MiningToolItemMixin(ToolMaterial material, Settings settings) {
+		super(material, settings);
+	}
 
 	@Inject(method = "getMiningSpeedMultiplier", at = @At("HEAD"))
 	public void cobWebsAreSpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-		axesareweapons$state = state;
+		axesareweapons$state.set(state);
 	}
 
 	@ModifyConstant(method = "getMiningSpeedMultiplier", constant = @Constant(floatValue = 1.0f))
 	public float cobWebsAreSpeed(float miningSpeed) {
-		float newMiningSpeed = overrideCobWebMiningSpeed((Item) (Object) this, axesareweapons$state, miningSpeed);
-		axesareweapons$state = null;
+		float newMiningSpeed = overrideCobWebMiningSpeed(this, axesareweapons$state.get(), miningSpeed);
+		axesareweapons$state.set(null);
 		return newMiningSpeed;
 	}
 
 	@Inject(method = "isSuitableFor", at = @At("HEAD"), cancellable = true)
 	public void cobWebsAreSuitable(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-		overrideCobWebSuitableness((Item) (Object) this, state, cir);
+		overrideCobWebSuitableness(this, state, cir);
 	}
 }
