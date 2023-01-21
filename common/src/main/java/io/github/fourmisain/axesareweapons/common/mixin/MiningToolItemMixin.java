c10fc002643663ee2e64a302b3d32b03e9ddb040
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-01-21 20:02:38 +0100

    backport 1.6.5

--- common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/MiningToolItemMixin.java
+++ common/src/main/java/io/github/fourmisain/axesareweapons/common/mixin/MiningToolItemMixin.java
@@ -1,16 +1,14 @@
 package io.github.fourmisain.axesareweapons.common.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.item.*;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.Vanishable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static io.github.fourmisain.axesareweapons.common.AxesAreWeaponsCommon.*;
+import static io.github.fourmisain.axesareweapons.common.AxesAreWeaponsCommon.isToolWeapon;
 
 @Mixin(value = MiningToolItem.class, priority = 990)
 public abstract class MiningToolItemMixin extends ToolItem implements Vanishable {
@@ -22,25 +20,5 @@ public abstract class MiningToolItemMixin extends ToolItem implements Vanishable
 	public int disableIncreasedAxeDurabilityLoss(int damageAmount) {
 		if (isToolWeapon(this)) return 1;
 		return damageAmount;
-	}
-
-	@Unique
-	BlockState axesareweapons$state;
-
-	@Inject(method = "getMiningSpeedMultiplier", at = @At("HEAD"))
-	public void cobWebsAreSpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-		axesareweapons$state = state;
-	}
-
-	@ModifyConstant(method = "getMiningSpeedMultiplier", constant = @Constant(floatValue = 1.0f))
-	public float cobWebsAreSpeed(float miningSpeed) {
-		float newMiningSpeed = overrideCobWebMiningSpeed(this, axesareweapons$state, miningSpeed);
-		axesareweapons$state = null;
-		return newMiningSpeed;
-	}
-
-	@Inject(method = "isSuitableFor", at = @At("HEAD"), cancellable = true)
-	public void cobWebsAreSuitable(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-		overrideCobWebSuitableness(this, state, cir);
 	}
 }
